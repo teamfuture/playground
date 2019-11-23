f28f54444b20a0c42a64c9b27045f73d9f4ec73c
Hugi Thordarson <hugi@karlmenn.is>
2019-11-23 18:40:17 +0000

    Filling closets with skeletons

---
+++ tng-kvc/src/main/java/tng/kvc/NGKeyValueCoding.java
@@ -0,0 +1,31 @@
+package tng.kvc;
+
+public interface NGKeyValueCoding {
+
+	public Object valueForKey( String key );
+
+	public void takeValueForKey( Object value, String key );
+
+	public static class Utility {
+
+		public static Object valueForKey( Object object, String key ) {
+
+			if( object instanceof NGKeyValueCoding ) {
+				return ((NGKeyValueCoding)object).valueForKey( key );
+			}
+
+			throw new IllegalStateException( "This should be implemented already" );
+
+			// method()
+			// _method()
+			// getMethod()
+			// _getMethod()
+			// variable
+			// _variable
+		}
+
+		public static void takeValueForKey( Object object, Object value, String key ) {
+
+		}
+	}
+}
---
+++ tng-kvc/src/test/java/tng/kvc/TestNGKeyValueCoding.java
@@ -0,0 +1,5 @@
+package tng.kvc;
+
+public class TestNGKeyValueCoding {
+
+}
